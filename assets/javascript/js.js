d933e5b3b94c8a1f2e3c15e35c5e44ac68ec2235
LuisRetana <luisretana123@gmail.com>
2021-07-22 15:07:32 -0400

    testing weather app

---
+++ assets/javascript/js.js
@@ -0,0 +1,14 @@
+$.getJSON(
+    "https://openweathermap.org/data/2.5/weather?q=Miami&appid=70b5cd96a908ec7ff606c26295b20d0c",
+    function(data){
+        console.log(data);
+
+        var icon ="http://openweathermap.org/img/w/" + data.weather[0].icon + "png";
+        var temp = data.main.temp;
+        var weather = data.weather[0].main;
+
+        $(".icon").attr("src",icon);
+        $(".weather").append(weather);
+        $(".temp").append(temp);
+    }
+);
