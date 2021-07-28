5170b40777e3e918abdc889cdab827897ef7e122
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-07-28 16:05:22 -0400

    created initial  HTML elements, created Javascript files to handle weather queries and local storage, i used namespaces since i was told that it would be the best way to keep it modular and not interfere with other teamates code, main js runs the code, the rest are utility services,  there are bugs that duplicate data display

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,32 +1,32 @@
-$.getJSON(
-    "https://api.openweathermap.org/data/2.5/weather?q=Miami&units=imperial&appid=70b5cd96a908ec7ff606c26295b20d0c",
-    function (data) {
-        console.log(data);
-
-        var icon =
-            "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        console.log(icon)
-        var temp = Math.floor(data.main.temp) + " Degrees";
-        var weather = data.weather[0].main;
-
-        $(".icon").attr("src", icon);
-        $(".weather").append(weather);
-        $(".temp").append(temp);
-    }
-);
-
-$.ajax({
-    type: "GET",
-    url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=B5AY8mCpUnhDPbrLaWNvgsTgbeF612rO",
-    async: true,
-    dataType: "json",
-    success: function (json) {
-        console.log(json);
-        const event = json._embedded.events[0].name
-        $(".events").append(event)
-       
-    },
-    error: function (xhr, status, err) {
-        // This time, we do not end up here!
-    }
-});
+// $.getJSON(
+//     "https://api.openweathermap.org/data/2.5/weather?q=Miami&units=imperial&appid=70b5cd96a908ec7ff606c26295b20d0c",
+//     function (data) {
+//         console.log(data);
+//
+//         var icon =
+//             "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+//         console.log(icon)
+//         var temp = Math.floor(data.main.temp) + " Degrees";
+//         var weather = data.weather[0].main;
+//
+//         $(".icon").attr("src", icon);
+//         $(".weather").append(weather);
+//         $(".temp").append(temp);
+//     }
+// );
+//
+// $.ajax({
+//     type: "GET",
+//     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=B5AY8mCpUnhDPbrLaWNvgsTgbeF612rO",
+//     async: true,
+//     dataType: "json",
+//     success: function (json) {
+//         console.log(json);
+//         const event = json._embedded.events[0].name
+//         $(".events").append(event)
+//
+//     },
+//     error: function (xhr, status, err) {
+//         // This time, we do not end up here!
+//     }
+// });
